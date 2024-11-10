21d12d15314828063da1f6e4a8e22d263aef0c81
hikae <account@egahika.dev>
2024-11-10 12:44:32 +0900

    improve prompt

--- src/prompt.tsx
+++ src/prompt.tsx
@@ -10,15 +10,22 @@ export interface PromptProps extends BasePromptElementProps {
   userQuery: string;
 }
 
+// https://prompts.chat/#act-as-an-english-translator-and-improver
+const actAsAnEnglishTranslatorAndImprover = `
+I want you to act as an English translator, spelling corrector and improver.
+I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English.
+I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences.
+Keep the meaning same, but make them more literary.
+Add the Japanese translation at the end of each response.
+I want you to only reply the correction, the improvements and nothing else, do not write explanations.
+`
+
 export class TranslatePrompt extends PromptElement<PromptProps, void> {
   render(_state: undefined, _sizing: PromptSizing) {
     return (
       <>
         <AssistantMessage>
-          You are a translator.
-          If given English text, translate it into Japanese.
-          Else if given text in any other language, translate it into both Japanese and English.
-          Output only the result.
+          ${actAsAnEnglishTranslatorAndImprover}
         </AssistantMessage>
         <UserMessage>{this.props.userQuery}</UserMessage>
       </>
