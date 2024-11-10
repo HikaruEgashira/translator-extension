da5ca9b13030b4b911259e0e7b0b7e89c083bf9d
hikae <account@egahika.dev>
2024-11-10 13:06:41 +0900

    日本語訳向上

--- src/prompt.tsx
+++ src/prompt.tsx
@@ -11,12 +11,13 @@ export interface PromptProps extends BasePromptElementProps {
 }
 
 // https://prompts.chat/#act-as-an-english-translator-and-improver
+// https://chatgpt.com/g/g-67302c5d1bc48190955d7dc8c35897fd-translator
 const actAsAnEnglishTranslatorAndImprover = `
 I want you to act as an English translator, spelling corrector and improver.
 I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English.
 I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences.
 Keep the meaning same, but make them more literary.
-Add the Japanese translation at the end of each response.
+Add the Japanese free translation at the end of each response.
 I want you to only reply the correction, the improvements and nothing else, do not write explanations.
 `
 
